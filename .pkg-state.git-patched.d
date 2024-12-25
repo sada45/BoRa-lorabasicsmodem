54dffd3b1e16c5dbf11428e5148d6cd4db43ef1d
emnetsliym <liym@emnets.org>
2024-12-25 15:39:09 +0800

    BoRa v1.0

---
+++ .pkg-state.git-patched.d
@@ -0,0 +1,8 @@
+/home/user/LORA/V1/RIOT/build/pkg/lorabasicsmodem/.pkg-state.git-patched: /home/user/LORA/V1/RIOT/pkg/lorabasics/patches/0001-smtc_ral-ral_defs-remove-const-from-radio_type-tcxo_.patch /home/user/LORA/V1/RIOT/pkg/lorabasics/patches/0002-treewide-fix-include-paths.patch /home/user/LORA/V1/RIOT/pkg/lorabasics/patches/0003-ral_sx1280-fix-unused-parameter-warning.patch /home/user/LORA/V1/RIOT/build/pkg/lorabasicsmodem/.pkg-state.git-downloaded  Makefile /home/user/LORA/V1/RIOT/pkg/pkg.mk /home/user/LORA/V1/RIOT/makefiles/utils/variables.mk
+/home/user/LORA/V1/RIOT/pkg/lorabasics/patches/0001-smtc_ral-ral_defs-remove-const-from-radio_type-tcxo_.patch:
+/home/user/LORA/V1/RIOT/pkg/lorabasics/patches/0002-treewide-fix-include-paths.patch:
+/home/user/LORA/V1/RIOT/pkg/lorabasics/patches/0003-ral_sx1280-fix-unused-parameter-warning.patch:
+/home/user/LORA/V1/RIOT/build/pkg/lorabasicsmodem/.pkg-state.git-downloaded:
+Makefile:
+/home/user/LORA/V1/RIOT/pkg/pkg.mk:
+/home/user/LORA/V1/RIOT/makefiles/utils/variables.mk:
